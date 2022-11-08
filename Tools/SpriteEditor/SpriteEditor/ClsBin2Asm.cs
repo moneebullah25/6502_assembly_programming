57ef10cf83ab923993661d13a9b701d4a63f33ce
moneebullah25 <58388634+moneebullah25@users.noreply.github.com>
2022-11-08 19:21:56 +0500

    Add files via upload

---
+++ Tools/SpriteEditor/SpriteEditor/ClsBin2Asm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    public static class ClsBin2Asm
+    {
+       public static string Bin2Asm(string filen, string txtLineStart,string Prefix, string DataFormat, string Suffix, string txtSeparator, string txtLineComment,int linemax)
+        {
+        
+        
+         if (filen!=""){
+                System.IO.BinaryReader BR=new System.IO.BinaryReader(System.IO.File.Open(filen, System.IO.FileMode.OpenOrCreate));
+                StringBuilder sb = new StringBuilder();
+
+                
+            int linecurrent = 0;
+            int linenum=0;
+            string LineByte = "0";
+            string linestart = txtLineStart;
+            long filelength=BR.BaseStream.Length;
+            while (filelength>0)
+            {
+                
+                if (linecurrent > 0) sb.Append(txtSeparator); else sb.Append(linestart);
+                sb.Append(Prefix);
+                if (DataFormat.ToLower() == "bin8") {
+                    byte b = BR.ReadByte();
+                    sb.Append(VbX.Dec2Bin8Bit(b));
+                    sb.Append(Suffix);
+                }
+                if (DataFormat.ToLower() == "bin16")
+                {
+                    byte b = BR.ReadByte();
+                    byte b2 = BR.ReadByte(); filelength--;
+                    sb.Append(VbX.Dec2Bin8Bit(b2));
+                    sb.Append(VbX.Dec2Bin8Bit(b));
+                    sb.Append(Suffix);
+                }
+                if (DataFormat.ToLower() == "bin16bigend")
+                {
+                    byte b = BR.ReadByte();
+                    byte b2 = BR.ReadByte(); filelength--;
+                    sb.Append(VbX.Dec2Bin8Bit(b));
+                    sb.Append(VbX.Dec2Bin8Bit(b2));
+                    sb.Append(Suffix);
+                }
+                if (DataFormat.ToLower() == "hex8" || DataFormat.ToLower() == "")
+                {
+                    byte b = BR.ReadByte();
+                    sb.Append(VbX.Dec2Hex8bit(b));
+                    sb.Append(Suffix);
+                }
+                if (DataFormat.ToLower() == "oct8")
+                {
+                    byte b = BR.ReadByte();
+                    sb.Append(VbX.Dec2Oct8Bit(b));
+                    sb.Append(Suffix);
+                }
+                if (DataFormat.ToLower() == "oct16")
+                {
+                    byte b = BR.ReadByte();
+                    byte b2 = BR.ReadByte(); filelength--;
+                    sb.Append(VbX.Dec2Oct16Bit(b+(b2*256)));
+                    sb.Append(Suffix);
+                }
+                if (DataFormat.ToLower() == "oct16bigend")
+                {
+                    byte b = BR.ReadByte();
+                    byte b2 = BR.ReadByte(); filelength--;
+                    sb.Append(VbX.Dec2Oct16Bit(b2 + (b * 256)));
+                    sb.Append(Suffix);
+                }
+                if (DataFormat.ToLower() == "dec8")
+                {
+                    byte b = BR.ReadByte();
+                    sb.Append(VbX.CStr(b));
+                    sb.Append(Suffix);
+                }
+                if (DataFormat.ToLower() == "dec16")
+                {
+                    byte b = BR.ReadByte();
+                    byte b2 = BR.ReadByte(); filelength--;
+                    sb.Append(VbX.CStr(b + (b2 * 256)));
+                    sb.Append(Suffix);
+                }
+                if (DataFormat.ToLower() == "dec16bigend")
+                {
+                    byte b = BR.ReadByte();
+                    byte b2 = BR.ReadByte(); filelength--;
+                    sb.Append(VbX.CStr(b2 + (b * 256)));
+                    sb.Append(Suffix);
+                }
+                if (DataFormat.ToLower() == "hex16")
+                {
+                    byte b = BR.ReadByte();
+                    byte b2 = BR.ReadByte(); filelength--;
+                    sb.Append(VbX.Dec2Hex8bit(b2));
+                    sb.Append(VbX.Dec2Hex8bit(b));
+                    sb.Append(Suffix);
+                }
+                if (DataFormat.ToLower() == "hex16bigend")
+                {
+                    byte b = BR.ReadByte();
+                    byte b2 = BR.ReadByte(); filelength--;
+                    sb.Append(VbX.Dec2Hex8bit(b));
+                    sb.Append(VbX.Dec2Hex8bit(b2));
+                    sb.Append(Suffix);
+
+                }
+                filelength--;
+                linecurrent++;
+                if (linecurrent >= linemax) {
+                    if (VbX.Trim(txtLineComment).Length > 0) { 
+                    sb.Append(txtLineComment);
+                    int spc = 3-linenum.ToString().Length; 
+                    if (spc>0) sb.Append(VbX.Right("   ",spc));
+                    sb.Append(linenum.ToString());
+                    // sb.Append("    (" + LineByte + ")");
+                    LineByte = BR.BaseStream.Position.ToString();
+                    linenum++;
+                    }
+                    linecurrent = 0; sb.Append(VbX.Chr(13) + VbX.Chr(10)); }
+                
+            }
+            
+            BR.Close();
+            return sb.ToString();
+            }
+
+         return ""; 
+        }
+        
+
+    }
+}
---
+++ Tools/SpriteEditor/SpriteEditor/ClsRLEFile.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class ClsRLEFile
+    {
+
+      
+        int OutDataPos = 0;
+
+        int lastByte = 256;
+        int thisByte = 0;
+        int RleCount = 0;
+        int RleCountPos = 0;
+        int LinearCount = 0;
+        int LinearCountPos = 0;
+        int mode = 0;
+
+        public void RLECompressFile(String SourceFile,String DestFile,int firstbyte,int bytestep){
+            System.IO.BinaryReader BR = new System.IO.BinaryReader(System.IO.File.Open(SourceFile, System.IO.FileMode.OpenOrCreate),Encoding.ASCII);
+            System.IO.BinaryWriter BW = new System.IO.BinaryWriter(System.IO.File.Open(DestFile, System.IO.FileMode.Create));
+            // System.IO.FileMode.Open),Encoding.ASCII
+            byte[] OutData = new byte[65536];
+            lastByte = 256;
+            thisByte = 0;
+            RleCount = 0;
+            RleCountPos = 0;
+            LinearCount = 0;
+            LinearCountPos = 0;
+            mode = 0;
+            OutDataPos = 0;
+
+            for (int i = 0; i < firstbyte; i++)
+            {
+                thisByte = BR.ReadByte();           // Skip starting bytes
+                //VbX.MsgBox(i.ToString());
+            }
+            thisByte = BR.ReadByte();
+
+            for (int i = 1; i < bytestep; i++)
+            {
+                if (BR.PeekChar() >= 0) BR.ReadByte();     // Skip bytes
+
+            }
+
+           if (thisByte != BR.PeekChar())
+           {
+               LinearCountPos = 0;
+               OutDataPos++;
+               LinearCount = 0;
+               mode = 1;
+           }
+
+            
+            while (BR.PeekChar()>=0){
+                lastByte=thisByte;
+
+                    thisByte = BR.ReadByte();
+                    //VbX.MsgBox(thisByte.ToString()+"  "+BR.BaseStream.Position.ToString());
+                if (thisByte!=lastByte){
+
+                    if (mode == 1 && LinearCount<127)
+                    {
+                        OutData[OutDataPos] = (byte)lastByte;
+                        OutDataPos++;
+                        LinearCount++;
+                    }
+                    else if (mode == 1)         // Reached 127 byte limit
+                    {
+                        OutData[LinearCountPos] = (byte)(LinearCount);
+                        LinearCountPos = OutDataPos;
+                        LinearCount = 0;
+                        OutDataPos++;
+                        OutData[OutDataPos] = (byte)lastByte;
+                        OutDataPos++;
+                        LinearCount++;
+                    }
+                    else {
+                        if (mode == 2) {
+                            OutData[RleCountPos] = (byte)(128 + RleCount);
+                                RleCount=0;
+                        }
+                        LinearCount = 0;
+                        LinearCountPos = OutDataPos;
+                        OutDataPos++;
+                        mode = 1;
+                    }
+                }
+                if (thisByte == lastByte)
+                {
+                    if (mode == 2 && RleCount < 127)
+                    {
+                        RleCount++;
+                    }
+                    else if (mode == 2)
+                    {
+                        OutData[RleCountPos] = (byte)(128 + RleCount);
+                        RleCountPos = OutDataPos;
+                        RleCount = 0;
+                        OutDataPos++;
+                        OutData[OutDataPos] = (byte)lastByte;
+                        OutDataPos++;
+                        RleCount++;
+
+                    }
+                    else
+                    {
+                        if (mode !=2)
+                        {
+                            OutData[LinearCountPos] = (byte)(LinearCount);
+                            if (LinearCount == 0 && OutDataPos>0) OutDataPos--; // No Linear data since last RLE
+                            LinearCount = 0;
+                            RleCount=2;
+                        }
+                        RleCountPos = OutDataPos;
+                        OutDataPos++;
+                        OutData[OutDataPos] = (byte)lastByte;
+                        OutDataPos++;
+                        
+                        mode = 2;
+                    }
+                }
+                for (int i = 1; i < bytestep; i++)
+                {
+                    if (BR.PeekChar() >= 0) BR.ReadByte();     // Skip bytes
+                     
+                }
+            }
+
+
+            if (mode == 1 && LinearCountPos>0)
+            {
+                
+                OutData[OutDataPos] = (byte)thisByte;
+                OutDataPos++;
+                LinearCount++;
+                 OutData[LinearCountPos] = (byte)(LinearCount);
+            }
+            if (mode == 2 && RleCount>0)
+            {
+                OutData[RleCountPos] = (byte)(128 + RleCount);
+                //OutData[RleCountPos + 1] = (byte)lastByte;
+            }
+            
+            //VbX.MsgBox(mode.ToString() + "  " + RleCount.ToString() + "   " + RleCountPos.ToString() + "    " + OutDataPos.ToString());
+            OutData[OutDataPos] = (byte)00;
+            //VbX.MsgBox(OutDataPos.ToString());
+            for (int i = 0; i <= OutDataPos; i++) {
+                BW.Write(OutData[i]);
+                
+            
+            }
+
+            BR.Close();
+            BW.Close();
+
+        }
+    }
+}
